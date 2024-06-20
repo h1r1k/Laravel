0221a9b08774f584627165f338c0f62f0d19f554
h1r1k <ha.2002.ta@icloud.com>
2024-06-21 01:25:52 +0900

    ブログ投稿の作成実装完了

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 */
 Route::get('/', function () { return view('welcome'); });
 Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class ,'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
 
 ?>
